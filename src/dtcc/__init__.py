b75253b79aeef9ddbb1c6920c92edaa75a36990a
Dag Wästberg <dwastberg@gmail.com>
2023-05-26 11:34:47 +0200

    setup meta package import

--- src/dtcc/__init__.py
+++ src/dtcc/__init__.py
@@ -1,5 +1,11 @@
 import dtcc_model
+import dtcc_model as model
 import dtcc_io
+import dtcc_io as io
+import dtcc_builder
+import dtcc_builder as builder
+import dtcc_viewer
+import dtcc_viewer as viewer
 
 # Collect __all__ from submodules
 modules = [dtcc_model, dtcc_io]
