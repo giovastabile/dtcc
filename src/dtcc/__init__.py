14d44c67c7edc81a27209a8df04a4f3d33d027aa
Anders Logg <logg@chalmers.se>
2023-08-30 11:11:33 +0200

    Add data module

--- src/dtcc/__init__.py
+++ src/dtcc/__init__.py
@@ -1,12 +1,13 @@
 import dtcc_model as model
 import dtcc_io as io
+import dtcc_data as data
 import dtcc_wrangler as wrangler
 import dtcc_builder as builder
 import dtcc_viewer as viewer
 
 
 # Collect __all__ from submodules
-modules = [model, io, wrangler, builder, viewer]
+modules = [model, io, data, wrangler, builder, viewer]
 __all__ = []
 for module in modules:
     for name in module.__all__:
