4b0a64c1e32470d4674b810567f5b67480f40d74
Anders Logg <logg@chalmers.se>
2023-09-04 15:16:26 +0200

    Small demo fix

--- demos/build_city_and_meshes.py
+++ demos/build_city_and_meshes.py
@@ -22,14 +22,24 @@ origin, bounds = calculate_bounds(buildings_path, pointcloud_path, p)
 
 # Load data from file
 city = load_city(data_directory / "PropertyMap.shp", bounds=bounds)
-point_cloud = load_pointcloud(data_directory, bounds=bounds)
+pointcloud = load_pointcloud(data_directory, bounds=bounds)
 
-# Build city
-city = build_city(city, point_cloud, bounds, p)
+# Build city model
+city = build_city(city, pointcloud, bounds, p)
+
+# Build ground mesh and building mesh (surface meshes)
+ground_mesh, building_mesh = build_mesh(city, p)
+
+# Build city mesh and volume mesh (tetrahedral mesh)
+mesh, volume_mesh = build_volume_mesh(city, p)
 
 # Save data to file
 city.save(data_directory / "city.pb")
+ground_mesh.save(data_directory / "ground_mesh.pb")
+building_mesh.save(data_directory / "building_mesh.pb")
+mesh.save(data_directory / "mesh.pb")
+volume_mesh.save(data_directory / "volume_mesh.pb")
 
 # View data
 city.view()
-point_cloud.view()
+pointcloud.view()
