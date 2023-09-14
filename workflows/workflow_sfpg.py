20f3b43756ca0a02a64be838401eb8fde94287fb
Vasilis Naserentin <vasilis.naserentin@chalmers.se>
2023-09-14 09:25:14 +0200

    Added initial workflow version of Anestis' MSc thesis code.

--- workflows/workflow_sfpg.py
+++ workflows/workflow_sfpg.py
@@ -1,5 +1,9 @@
-# Copyright (C) 2023 Firstname Lastname
+# Copyright (C) 2023 Anestis K., George S., V. Naserentin
 # Licensed under the MIT License
 #
 # This script documents the workflow for SFPG.
 # https://github.com/dtcc-platform/dtcc/issues/56
+
+# Initially we are adding the working code from Anestis' MSc:
+
+
