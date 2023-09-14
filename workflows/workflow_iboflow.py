1aa2fa5843e8098c867c95e1a4e6f3b528978f89
Vasilis Naserentin <125367195+vbassn@users.noreply.github.com>
2023-09-14 10:46:21 +0300

    Update workflow_iboflow.py

--- workflows/workflow_iboflow.py
+++ workflows/workflow_iboflow.py
@@ -1,5 +1,7 @@
-# Copyright (C) 2023 Firstname Lastname
+# Copyright (C) 2023 Vasilis Naserentin, George Spaias
 # Licensed under the MIT License
 #
 # This script documents the workflow for IBOFlow.
 # https://github.com/dtcc-platform/dtcc/issues/53
+
+# API connection to SMHI
