099652dd9aaa8751393fea3e20cabbbe89a9effb
Anders Logg <logg@chalmers.se>
2023-10-05 11:19:07 +0200

    Sketch new API for load/build

---
+++ sandbox/new_api.py
@@ -0,0 +1,21 @@
+from dtcc import *
+
+# Load stuff
+footprints = load_footprints("footprints.shp")
+pointcloud = load_pointcloud("pointcloud.las")
+
+# Build stuff
+terrain = build_terrain(pointcloud)
+buildings = build_buildings(footprints, pointcloud)
+
+# Create city
+city = City()
+city.terrain = terrain
+city.buildings = buildings
+
+# Shortcut (builds and sets data)
+city = build_city(footprints, pointcloud)
+
+# How to handle roads?
+# How to handle trees?
+# How to handle water?
